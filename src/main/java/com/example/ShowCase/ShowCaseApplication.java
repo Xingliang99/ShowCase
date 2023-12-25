2a38dc60bf35319ef5b2ac8f61242dda14959853
Xingliang99 <antonyliu99@gmail.com>
2023-12-25 10:01:02 +0800

    Initial commit

---
+++ src/main/java/com/example/ShowCase/ShowCaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.ShowCase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowCaseApplication.class, args);
+    }
+
+}
